c8d38928d5a6930077decb55f8616462f6298666
jxxtty <jaehee941106@gmail.com>
2022-03-22 16:40:33 +0900

    war

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.5.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("war") // war 추가
 
 	val kotlinVersion = "1.5.21"
 
